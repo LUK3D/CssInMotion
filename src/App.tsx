10944830968aa77266ec0179eeaafb110a2f69d1
Filipe Lukebana <filipelukebana@gmail.com>
2022-09-26 19:54:22 -0300

    🪲 Typescript Type error fixes

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,10 @@ function App() {
               <div className='w-full flex layers p-2 border-b border-dark-900'>
                  <p>LAYERS</p>
               </div>
-              <div className='h-full overflow-y-auto' id='layers' onScroll={(e)=>setScrollY(e.target.scrollTop)}>
+              <div className='h-full overflow-y-auto' id='layers' onScroll={(e)=>setScrollY(
+                //@ts-ignore
+                e.target.scrollTop
+                )}>
               {
                 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16].map((key)=><Layer text="Shape01" selector={`${key}`} key={`layer_${key}`}></Layer>)
               }
@@ -39,7 +42,10 @@ function App() {
                   frames.map((x)=><p>{x}s</p>)
                 }
               </div>
-              <div className='flex flex-col overflow-y-auto h-full ' id='layers_frames' onScroll={(e)=>setScrollY(e.target.scrollTop)}>
+              <div className='flex flex-col overflow-y-auto h-full ' id='layers_frames' onScroll={(e)=>setScrollY(
+                //@ts-ignore
+                e.target.scrollTop
+                )}>
               {
                 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16].map((key)=><LayerFrames selector={key.toString()} key={'keys_'+key}></LayerFrames>)
               }
