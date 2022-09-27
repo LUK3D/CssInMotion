097f27ca592aa9def4ebf87432a4b5dc2a14f29c
Filipe Lukebana <filipelukebana@gmail.com>
2022-09-27 17:53:10 -0300

    🔥 Border Property UI Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, ColorInput, NumberInput, Slider, TextInput, Tooltip } from '@mantine/core';
+import { Checkbox, ColorInput, NumberInput, SegmentedControl, Slider, TextInput, Tooltip } from '@mantine/core';
 import { useState } from 'react';
 import './App.css'
 import { CIMotionDropZone } from './components/dropZone';
@@ -246,6 +246,53 @@ function App() {
                         </div>
                       </div>
                      
+                    </div>
+                  }
+                  ></Poperty>
+
+                <Poperty tittle='Boborder'
+                  body={
+                    <div className='w-full p-5 flex flex-col'>
+                      <div className='flex items-center justify-between mb-3'>
+                        <div className='w-2/6 flex items-center'>
+                          <p>Types</p>
+                        </div>
+                        <div className='w-4/5 flex'>
+                          <SegmentedControl
+                          className='w-full bg-dark-900'
+                              data={[
+                                { label: < div className='w-full h-h-full justify-center items-center'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                  <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                </svg>
+
+                                </ div>, value: 'none' },
+                                { label: '-----', value: 'dashed' },
+                                { label: 
+                                <div className='flex justify-center items-center h-[25px] w-full'>
+                                  <div className='w-7 h-0.5 bg-gray-400'></div>
+                                </div>, value: 'solid' },
+                              ]}
+                            />
+                        </div>
+                      </div>
+                      <div className='flex items-center justify-between mb-3'>
+                        <div className='w-2/6 flex items-center'>
+                          <p>Color</p>
+                        </div>
+                        <div className='w-4/5 '>
+                        <ColorInput format="rgb"  swatches={['#25262b', '#868e96', '#fa5252', '#e64980', '#be4bdb', '#7950f2', '#4c6ef5', '#228be6', '#15aabf', '#12b886', '#40c057', '#82c91e', '#fab005', '#fd7e14']} />
+                        </div>
+                      </div>
+
+                      <div className='flex items-center justify-between mb-3'>
+                        <div className='w-2/6 flex items-center'>
+                          <p>Radius</p>
+                        </div>
+                        <div className='w-4/5 '>
+                        <Slider/>
+                        </div>
+                      </div>
                     </div>
                   }
                   ></Poperty>
